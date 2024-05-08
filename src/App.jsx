190894cfcf9aee802f3ca93a9d2ad32bdac6e8d0
knottykit <kit.collene@gmail.com>
2024-05-08 16:04:11 +0800

    initial

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Benefits from "./components/Benefits";
+import Button from "./components/Button";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+        <Benefits />
+      </div>
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
