f5e8d368901228f2fa051a20fc41ad18acc4a707
knottykit <kit.collene@gmail.com>
2024-05-10 13:45:09 +0800

    added pricing roadmap footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import ButtonGradient from "./assets/svg/ButtonGradient";
 import Benefits from "./components/Benefits";
 import Button from "./components/Button";
 import Collaboration from "./components/Collaboration";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
 import Services from "./components/Services";
 
 const App = () => {
@@ -17,6 +19,8 @@ const App = () => {
         <Collaboration />
         <Services />
         <Pricing />
+        <Roadmap />
+        <Footer />
       </div>
       <ButtonGradient />
     </>
--- src/components/PricingList.jsx
+++ src/components/PricingList.jsx
@@ -9,7 +9,7 @@ const PricingList = () => {
       {pricing.map((item) => (
         <div
           id={item.id}
-          className="w-[19rem] max-lg:w-full h-full px-6 bg-n-8 border border-n-6 rounded-[2rem] lg:w-auto even:py-14 odd:py-8"
+          className="w-[19rem] max-lg:w-full h-full px-6 bg-n-8 border border-n-6 rounded-[2rem] lg:w-auto even:py-14 odd:py-8 [&>h4]:first:text-color-2   [&>h4]:even:text-color-1 [&>h4]:last:text-color-3"
         >
           <h4 className="h4 mb-4">{item.title}</h4>
           <p className="body-2 min-h-[4rem] mb-3 text-n-1/50">
